67193ed460c987428841c7d233b37dbb832bfd06
Partha Sardar <partha746@gmail.com>
2019-12-28 08:56:20 +0530

    Added ultrasonic Sersor
    - Added HW Time to Blynk dash
    - Changed water level to percentage
    - Added Soft reboot button in Blynk dash

--- Soil_mois.ino
+++ Soil_mois.ino
@@ -8,8 +8,10 @@
 #include "wifi_passphrares.h" //includes variables SSID1, WifiPass, auth[] & *GScriptId
 
 #define SMSensor A0
-#define Relay D1
 #define LED D0
+#define Relay D1
+#define TRIGGERPIN D2
+#define ECHOPIN D3
 
 BlynkTimer timer;
 
@@ -36,6 +38,8 @@ int wateringTime = 8; //24 Hrs clock time // Time after to start watering plants
 int chkWPTimer = 5*30000UL; //Watering plants timer 
 int NRDelay = 15*60000UL; //Watering plants timer 
 int AHDelay = 1*60*60000UL;
+int LWL = 45; //Lower water level
+int HWL = 12; //Lower water level
 
 void OTA(){
   ArduinoOTA.setHostname("Plantwatering_balc_out");
@@ -71,35 +75,68 @@ void OTA(){
   ArduinoOTA.begin();
 }
 
+float sonarDist(){
+  long duration, distance;
+  digitalWrite(TRIGGERPIN, LOW);  
+  delayMicroseconds(3); 
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(12);   
+  digitalWrite(TRIGGERPIN, LOW);
+  duration = pulseIn(ECHOPIN, HIGH);
+  distance = (duration/2) / 29.1;
+  float WL = (((LWL - HWL)-(HWL - distance)) / (LWL - HWL)) * 100;
+  
+  return WL;
+}
+
 void blynkConnect()
 {
   Blynk.begin(auth, SSID1, WifiPass);
 }
 
 void reportSoilMois(){
-  timeClient.update();
-  if (timeClient.getHours() == wateringTime){
-    Serial.println("watering Plants");
-    digitalWrite (Relay, HIGH);
-    led.on();
+  if (sonarDist() <= LWL){
     timeClient.update();
-    blynkConnect();
-    Blynk.notify("Starting Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
-    delay(180000UL);
-
-    Serial.println("watering Plants Done");
-    digitalWrite (Relay, LOW);
-    timeClient.update();
-    blynkConnect();
-    Blynk.notify("Stopping Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
-    led.off();
-    delay(AHDelay);
+    if (timeClient.getHours() == wateringTime){
+      Serial.println("watering Plants");
+      digitalWrite (Relay, HIGH);
+      led.on();
+      timeClient.update();
+      blynkConnect();
+      Blynk.notify("Starting Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
+      Blynk.virtualWrite(V2, HIGH);
+      delay(180000UL);
+  
+      Serial.println("watering Plants Done");
+      digitalWrite (Relay, LOW);
+      timeClient.update();
+      blynkConnect();
+      Blynk.virtualWrite(V2, LOW);
+      Blynk.notify("Stopping Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
+      led.off();
+      delay(AHDelay);
+    }
   }
-
+  else
+  {
+    blynkConnect();
+    Blynk.notify("Low water tank level @ " + timeClient.getFormattedTime() + "!!!!!");    
+  }
+  
   sensor_analog = analogRead(SMSensor);
   moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
   blynkConnect();
   Blynk.virtualWrite(V1, moisture_percentage);
+  Blynk.virtualWrite(V3, sonarDist());
+}
+
+void reportwaterLevel(){
+  Blynk.virtualWrite(V3, sonarDist());
+}
+
+void reporthwTime(){
+  timeClient.update();
+  Blynk.virtualWrite(V4, timeClient.getFormattedTime());
 }
 
 void setup() {
@@ -116,6 +153,9 @@ void setup() {
   pinMode(Relay, OUTPUT);
   pinMode(SMSensor, INPUT);
 
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+
   Serial.println("Connecting to wifi: ");
   while (wifiMulti.run() != WL_CONNECTED) {
     delay(500);
@@ -130,14 +170,21 @@ void setup() {
   led.off();
 
   timer.setInterval(NRDelay, reportSoilMois);
+  timer.setInterval(NRDelay, reportwaterLevel);
+  timer.setInterval(1000, reporthwTime);
 
   timeClient.begin();
 }
 
 BLYNK_WRITE(V2)
 {
-  if(param.asInt() == 1){digitalWrite(Relay, HIGH); led.on();}
-  else if (param.asInt() == 0){digitalWrite(Relay, LOW); led.off();}
+  if(param.asInt() == 1){digitalWrite(Relay, HIGH); led.on(); Blynk.virtualWrite(V3, sonarDist());}
+  else if (param.asInt() == 0){digitalWrite(Relay, LOW); led.off(); Blynk.virtualWrite(V3, sonarDist());}
+}
+
+BLYNK_WRITE(V5)
+{
+  if(param.asInt() == 1){ESP.restart();}
 }
 
 void loop() {
