dc6c3690077f842aed86667392730ce3fbbc4daf
Partha Sardar <partha746@gmail.com>
2019-12-31 03:05:10 +0530

    Added a time selector in Blynk
    - Changed hard codded variable for watering plants timer to variable synced with blynk

--- Soil_mois.ino
+++ Soil_mois.ino
@@ -12,33 +12,23 @@
 #define Relay D1
 #define TRIGGERPIN D2
 #define ECHOPIN D3
-
-BlynkTimer timer;
-
 WidgetLED led(V0);
-WidgetTerminal terminal(V1);
-  
+
+BlynkTimer timer;  
 ESP8266WiFiMulti wifiMulti;
 WiFiUDP ntpUDP;
 
-const long utcOffsetInSeconds = 19800;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-
-const char* host = "script.google.com";
-const char* googleRedirHost = "script.googleusercontent.com";
-const int httpsPort = 443;
-
-String relayStatus;
-String url;
 float moisture_percentage;
 int sensor_analog;
-int systemStarted = millis();
 int wateringTime = 9; //24 Hrs clock time // Time after, to start watering plants
 int chkWPTimer = 2*30000UL; //Watering plants timer 
 int NRDelay = 15*60000UL; //Delay between every read check
 int AHDelay = 1*60*60000UL; //Delay once watered
 float LWL = 45.0; //Lower water level
 float HWL = 12.0; //High water level
+const long utcOffsetInSeconds = 19800;
+
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 void OTA(){
   ArduinoOTA.setHostname("Plantwatering_balc_out");
@@ -83,7 +73,6 @@ float sonarDist(){
   digitalWrite(TRIGGERPIN, LOW);
   duration = pulseIn(ECHOPIN, HIGH);
   distance = (duration/2) / 29.1;
-
   float WL = ((LWL - distance) / (LWL - HWL)) * 100;
   
   return WL;
@@ -104,23 +93,24 @@ void waterPlants(){
   if (sonarDist() >= 5.0){
     timeClient.update();
     if (timeClient.getHours() == wateringTime){
-      Serial.println("watering Plants");
+      timeClient.update();
       digitalWrite (Relay, HIGH);
-      reportSoilMoisture;
+      reportSoilMoisture();
+      blynkConnect();
       led.on();
-      timeClient.update();
-      blynkConnect();
-      Blynk.notify("Starting Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
       Blynk.virtualWrite(V2, HIGH);
-      delay(180000UL);
+      Blynk.notify("Starting Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
+
+      delay(chkWPTimer);
   
-      Serial.println("watering Plants Done");
-      digitalWrite (Relay, LOW);
       timeClient.update();
+      digitalWrite (Relay, LOW);
+      reportSoilMoisture();
       blynkConnect();
+      led.off();
       Blynk.virtualWrite(V2, LOW);
       Blynk.notify("Stopping Watering Plants @ " + timeClient.getFormattedTime() + "!!!!!");
-      led.off();
+
       delay(AHDelay);
     }
   }
@@ -129,7 +119,7 @@ void waterPlants(){
     Blynk.notify("Low water tank level @ " + timeClient.getFormattedTime() + "!!!!!");    
   }
   
-  reportSoilMoisture;
+  reportSoilMoisture();
   Blynk.virtualWrite(V3, sonarDist());
 }
 
@@ -180,8 +170,16 @@ void setup() {
 }
 
 BLYNK_WRITE(V2){
-  if(param.asInt() == 1){digitalWrite(Relay, HIGH); led.on(); Blynk.virtualWrite(V3, sonarDist());}
-  else if (param.asInt() == 0){digitalWrite(Relay, LOW); led.off(); Blynk.virtualWrite(V3, sonarDist());}
+  if(param.asInt() == 1){
+    digitalWrite(Relay, HIGH);
+    led.on();
+    Blynk.virtualWrite(V3, sonarDist());
+  }
+  else if (param.asInt() == 0){
+    digitalWrite(Relay, LOW); 
+    led.off(); 
+    Blynk.virtualWrite(V3, sonarDist());
+  }
 }
 
 BLYNK_WRITE(V5){
@@ -196,6 +194,10 @@ BLYNK_WRITE(V6){
 
 BLYNK_WRITE(V7){
   chkWPTimer = param.asInt() * 60000UL;
+}
+
+BLYNK_WRITE(V8){
+  wateringTime = param.asInt();
 }
 
 void loop() {
